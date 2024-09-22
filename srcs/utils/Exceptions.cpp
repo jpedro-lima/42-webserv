f97b84d68cc468b64392fee44b4c3fbc316eff43
João Pedro <jpedrones@hotmail.com>
2024-09-22 20:52:05 -0300

    [Exceptions]: update Exceptions

--- srcs/utils/Exceptions.cpp
+++ srcs/utils/Exceptions.cpp
@@ -6,7 +6,7 @@
 /*   By: joapedr2 < joapedr2@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 12:37:43 by joapedr2          #+#    #+#             */
-/*   Updated: 2024/09/22 13:23:24 by joapedr2         ###   ########.fr       */
+/*   Updated: 2024/09/22 20:22:57 by joapedr2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,10 @@ const char	*Exceptions::ConfigFileEmptyException::what() const throw() {
 	return "Exception thrown: Configuration file is empty";
 }
 
+const char	*Exceptions::ExpectedCurlyBracketsAfter::what() const throw() {
+	return "Error: expected '{' after server directive.";
+}
+
+const char	*Exceptions::ExpectedCurlyBracketsBefore::what() const throw() {
+	return "Error: expected '{' after server directive.";
+}
--- srcs/utils/Exceptions.hpp
+++ srcs/utils/Exceptions.hpp
@@ -6,7 +6,7 @@
 /*   By: joapedr2 < joapedr2@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 12:37:39 by joapedr2          #+#    #+#             */
-/*   Updated: 2024/09/22 13:23:42 by joapedr2         ###   ########.fr       */
+/*   Updated: 2024/09/22 20:20:15 by joapedr2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,27 @@
 class Exceptions {
 public:
 	class	FileNotFoundException: public std::exception{
-		public:
+	public:
 		virtual const char	*what() const throw();
 	};
 
 	class	ConfigFileErrorException: public std::exception{
-		public:
+	public:
 		virtual const char	*what() const throw();
 	};
 	
 	class	ConfigFileEmptyException: public std::exception{
-		public:
+	public:
+		virtual const char	*what() const throw();
+	};
+	
+	class	ExpectedCurlyBracketsAfter: public std::exception{
+	public:
+		virtual const char	*what() const throw();
+	};
+	
+	class	ExpectedCurlyBracketsBefore: public std::exception{
+	public:
 		virtual const char	*what() const throw();
 	};
 };
